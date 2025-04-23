fd1490dce7e0306df7b4b5d2d7a38989c8c4dc87
SANTIAGO IGAL DOBROVITZKY <49317356@ort>
2025-04-23 08:02:44 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP03_Dobrovitzky_Tanel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
